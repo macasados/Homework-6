fae8e244e0cdc907370bdacb9f266ddabf9b03ad
macasados <112190950+macasados@users.noreply.github.com>
2022-10-04 04:36:28 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+//create variables
+  double p, H, Q;
+  
+  
+}
+
+double Generator (double H, double Q)
+{
+  cout << "Enter value for flow rate volume (m3/s): " << endl;
+  cin >> Q;
+  cout << "Enter value for head distance (meters) " << endl; 
+  cin >> H;
+    double P = ((997)*(Q)*(H)*(9.81));
+  return p;
+  
+}
