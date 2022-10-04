1d2c12258672be3ea7380b10eafe22bd21dbe065
macasados <112190950+macasados@users.noreply.github.com>
2022-10-04 05:04:40 +0000

    Run the function and display output

--- main.cpp
+++ main.cpp
@@ -3,16 +3,22 @@
 #include <iomanip>
 
 using namespace std;
+double PowerGen (double H, double Q);
 
 int main() {
-
+  
 //create variables
   double p, H, Q;
-  
+
+  //run the created function
+  p = PowerGen (H, Q);
+
+ // display the output
+  cout << p << endl;
   
 }
 
-double Generator (double H, double Q)
+double PowerGen (double H, double Q)
 {
   cout << "Enter value for flow rate volume (m3/s): " << endl;
   cin >> Q;
